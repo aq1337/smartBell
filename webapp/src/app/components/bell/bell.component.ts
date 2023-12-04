9c1ec3bba58c8e1ee8c461e6a0104dd25c20d33d
aq1337 <43763226+aq1337@users.noreply.github.com>
2023-12-04 22:36:40 +0100

    initial setup

---
+++ webapp/src/app/components/bell/bell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bell',
+  standalone: true,
+  imports: [],
+  templateUrl: './bell.component.html',
+  styleUrl: './bell.component.css'
+})
+export class BellComponent {
+
+}
---
+++ webapp/src/app/components/opener/opener.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opener',
+  standalone: true,
+  imports: [],
+  templateUrl: './opener.component.html',
+  styleUrl: './opener.component.css'
+})
+export class OpenerComponent {
+
+}
